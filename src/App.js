b57b262b9fa3b152c3df1cd7cf179f80b72ee93c
Viktor <lulzsec123@yahoo.com>
2023-03-20 15:07:27 +0200

    Fixed scss not rendering issue

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
+import Header from './components/Header';
+import './default.scss';
+
 function App() {
   return (
     <div className="App">
+      <Header />
      <h1>yes</h1>
     </div>
   );
---
+++ src/components/Header/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './styles.scss';
+
+const Header = (props) =>{
+    return(
+        <header className='header'>
+        zdr
+        </header>
+    );
+};
+
+export default Header;
