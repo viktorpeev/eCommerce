68d70563ad755caadd136691b9bd8f1aff01cfd0
Viktor <lulzsec123@yahoo.com>
2023-03-23 16:23:08 +0200

    Styled the homepage section images

--- src/components/Directory/index.js
+++ src/components/Directory/index.js
@@ -7,12 +7,12 @@ const Directory = (props) => {
     return (
         <div className="Directory">
             <div className="wrap">
-                <div className="item" style={{ backgroundImage: `url(${Male})` }}>
+                <div className="item" style={{ backgroundImage:`linear-gradient( rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5) ), url(${Male})` }}>
                     <a href="/">
                         Shop Men
                     </a>
                 </div>
-                <div className="item" style={{ backgroundImage: `url(${Female})` }}>
+                <div className="item" style={{ backgroundImage:`linear-gradient( rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5) ),url(${Female})` }}>
                     <a href="/">
                         Shop Women
                         </a>
