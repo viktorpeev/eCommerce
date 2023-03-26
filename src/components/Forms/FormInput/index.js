b986099daa7020686dfcc21e0bbd3786e333314b
Viktor <lulzsec123@yahoo.com>
2023-03-26 12:57:31 +0300

    added input fields to register form

---
+++ src/components/Forms/FormInput/index.js
@@ -0,0 +1,15 @@
+const FormInput = ({handleChange, label, ...otherProps}) =>{
+    return(
+        <div className='formRow'>
+            {label && (
+                <label>
+                    {label}
+                </label>
+            )}
+
+            <input className="formInput" onChange={handleChange} {...otherProps}/>
+        </div>
+  );
+};
+
+export default FormInput;
--- src/pages/Registration/index.js
+++ src/pages/Registration/index.js
@@ -1,11 +1,61 @@
+import React, {useState } from "react";
+import './styles.scss';
 
-
+import FormInput from './../../components/Forms/FormInput';
+import Button from './../../components/Forms/Button';
 const Registration = (proops) => {
+    const [state,setState] = useState({
+        displayName:'',
+        email:'',
+        password:'',
+        confirmPassword:''
+    });
+
+    const {displayName,email,password,confirmPassword} = state;
+
+    const handleChange = event => {
+        const { name, value } = event.target;
+        setState({ ...state, [name]: value });
+    };
+
     return (
-        <div>
-            <h1>
-                registratin page
-            </h1>
+        <div className='registration'>
+            <div className='wrap'>
+                <h1>Signup</h1>
+            </div>
+            <form>
+                <FormInput
+                    type="text"
+                    name="displayName"
+                    value={displayName}
+                    placeholder="Name"
+                    onChange={handleChange}
+                />
+                <FormInput
+                    type="text"
+                    name="email"
+                    value={email}
+                    placeholder="Email"
+                    onChange={handleChange}
+                />
+                <FormInput
+                    type="text"
+                    name="password"
+                    value={password}
+                    placeholder="Password"
+                    onChange={handleChange}
+                />
+                <FormInput
+                    type="text"
+                    name="password"
+                    value={confirmPassword}
+                    placeholder="Confirm password"
+                    onChange={handleChange}
+                />
+                <Button type='submit'>
+                    Register
+                </Button>
+            </form>
         </div>
     );
 };
