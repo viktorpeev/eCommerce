b4f7358ae74f1fe7b24072e92822a5bc529ca07f
Viktor <lulzsec123@yahoo.com>
2023-03-27 16:22:33 +0300

    Implemented login functionality

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,14 +1,52 @@
+import { useState } from "react";
 import Button from "../../components/Forms/Button";
-import { signInWithGoogle } from "../../firebase/utilis";
+import { signInWithGoogle, auth } from "../../firebase/utilis";
+
+import FormInput from './../../components/Forms/FormInput';
 
 const Login = (props) => {
 
+    const initialState={
+        email:'',
+        password:''
+    }
+    const [state,setState] = useState(initialState);
+    const {email,password} = state;
+
+    const handleChange = event => {
+        const { name, value } = event.target;
+        setState({ ...state, [name]: value });
+
+    };
+
+
     const handleSubmit = async e =>{
         e.preventDefault();
+        try{
+            await auth.signInWithEmailAndPassword(email,password);
+            setState(initialState);
+        } catch(err){
+            console.log(err);
+        }
     }
-
+    
     return (
         <form onSubmit={handleSubmit}>
+            <FormInput
+            type='email'
+            name='email'
+            value={email}
+            placeholder='Email'
+            onChange={handleChange}
+            />
+            <FormInput
+            type='password'
+            name='password'
+            value={password}
+            placeholder='Password'
+            onChange={handleChange}
+            />
+             <Button onClick={handleSubmit}>Sign in</Button>
             <Button onClick={signInWithGoogle}>Sign in with Google</Button>
         </form>
     );
